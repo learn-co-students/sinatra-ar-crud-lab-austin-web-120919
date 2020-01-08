daced0a52fe4fbbed7871ca59ef2bce14d3a8c71
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-08 15:10:16 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+
+  get '/articles/new' do
+    erb :new
   end
+
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    # binding.pry
+    @article = Article.find(params[:id].to_i)
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    #binding.pry
+    @article = Article.find(params[:id].to_i)
+    #binding.pry
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id].to_i)
+    @article.update(params.except(:_method))
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete '/articles/:id' do
+    Article.find(params[:id].to_i).delete
+    redirect to "/articles"
+  end
+
 end
